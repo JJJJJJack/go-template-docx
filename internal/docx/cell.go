20cb0b8e3f410db0ee6356def0f14949b815e91b
JJJJJJack <97jack@live.it>
2025-06-03 02:31:14 +0200

    debug prints

--- internal/docx/cell.go
+++ internal/docx/cell.go
@@ -43,7 +43,7 @@ func replaceIndexesWithValuesFromSharedStrings(fileContent []byte, values map[in
 	re := regexp.MustCompile(`<c[^>]*t="s"[^>]*>.*?<v>(\d+)</v>.*?</c>`)
 	matches := re.FindAllStringSubmatch(string(fileContent), -1)
 
-	for i, match := range matches {
+	for _, match := range matches {
 
 		n, err := strconv.Atoi(match[1])
 		if err != nil {
@@ -51,15 +51,13 @@ func replaceIndexesWithValuesFromSharedStrings(fileContent []byte, values map[in
 		}
 
 		if value, ok := values[n]; ok {
-			fmt.Println(i, match)
-			// fmt.Println("Replacing index", n, "with value", values[n])
-			// remove attribute `t="s"`
-			// and the value with the value from shared strings
 			removedRefToSharedString := strings.Replace(match[0], `t="s"`, "", 1)
+
 			oldV := fmt.Sprintf("<v>%d</v>", n)
 			newV := fmt.Sprintf("<v>%s</v>", value)
-			fmt.Println(i, "Replacing", oldV, "with", newV)
+
 			replace := strings.Replace(removedRefToSharedString, oldV, newV, 1)
+
 			fileContent = bytes.Replace(fileContent, []byte(match[0]), []byte(replace), 1)
 		}
 	}
