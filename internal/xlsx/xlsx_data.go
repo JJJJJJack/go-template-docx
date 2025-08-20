a68ad81312526a99ad5a991688b88b3c4b573774
JJJJJJack <97jack@live.it>
2025-08-20 19:32:50 +0200

    typo

--- internal/xlsx/xlsx_data.go
+++ internal/xlsx/xlsx_data.go
@@ -5,7 +5,7 @@ type XlsxData struct {
 }
 type XlsxFile map[string]XlsxData
 
-// used to store xlsx charts values accross multiple files processings
+// used to store xlsx charts values across multiple files processings
 // instead of argument drilling down the values in each function
 // ...any better idea? I hate globals and objects referenced everywhere
 var XlsxFiles = make(XlsxFile)
