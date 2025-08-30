8f387182dc983dd5f55b4362bb6e985b53907078
JJJJJJack <97jack@live.it>
2025-08-31 00:46:04 +0200

    unused centred image draft

--- internal/docx/template_funcs.go
+++ internal/docx/template_funcs.go
@@ -11,76 +11,6 @@ type XmlImageData struct {
 	RefID   string
 }
 
-const imageCenterTemplateXml = `<w:p w14:paraId="4E9843FD" w14:textId="77777777" w:rsidR="00963E01" w:rsidRDefault="0028620A"
-      w:rsidP="00963E01">
-      <w:pPr>
-        <w:keepNext />
-        <w:spacing w:after="0" w:line="240" w:lineRule="auto" />
-        <w:jc w:val="center" />
-      </w:pPr>
-      <w:r>
-        <w:rPr>
-          <w:rFonts w:eastAsia="Calibri" />
-          <w:noProof />
-          <w:u w:val="single" />
-        </w:rPr>
-        <w:lastRenderedPageBreak />
-        <w:drawing>
-          <wp:inline distT="0" distB="0" distL="0" distR="0" wp14:anchorId="68DC1111"
-            wp14:editId="381D76A9">
-            <wp:extent cx="2543175" cy="2962275" />
-            <wp:effectExtent l="0" t="0" r="0" b="0" />
-            <wp:docPr id="1228041855" name="Picture 1" />
-            <wp:cNvGraphicFramePr>
-              <a:graphicFrameLocks xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"
-                noChangeAspect="1" />
-            </wp:cNvGraphicFramePr>
-            <a:graphic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
-              <a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture">
-                <pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture">
-                  <pic:nvPicPr>
-                    <pic:cNvPr id="0" name="Picture 1" />
-                    <pic:cNvPicPr>
-                      <a:picLocks noChangeAspect="1" noChangeArrowheads="1" />
-                    </pic:cNvPicPr>
-                  </pic:nvPicPr>
-                  <pic:blipFill>
-                    <a:blip r:embed="rId10">
-                      <a:extLst>
-                        <a:ext uri="{28A0092B-C50C-407E-A947-70E740481C1C}">
-                          <a14:useLocalDpi
-                            xmlns:a14="http://schemas.microsoft.com/office/drawing/2010/main"
-                            val="0" />
-                        </a:ext>
-                      </a:extLst>
-                    </a:blip>
-                    <a:srcRect />
-                    <a:stretch>
-                      <a:fillRect />
-                    </a:stretch>
-                  </pic:blipFill>
-                  <pic:spPr bwMode="auto">
-                    <a:xfrm>
-                      <a:off x="0" y="0" />
-                      <a:ext cx="2543175" cy="2962275" />
-                    </a:xfrm>
-                    <a:prstGeom prst="rect">
-                      <a:avLst />
-                    </a:prstGeom>
-                    <a:noFill />
-                    <a:ln>
-                      <a:noFill />
-                    </a:ln>
-                  </pic:spPr>
-                </pic:pic>
-              </a:graphicData>
-            </a:graphic>
-          </wp:inline>
-        </w:drawing>
-      </w:r>
-    </w:p>
-		`
-
 const imageTemplateXml = `<w:drawing>
   <wp:inline distT="0" distB="0" distL="0" distR="0"
     xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"
@@ -129,10 +59,6 @@ func image(filename string) string {
 
 func replaceImage(filename string) string {
 	return fmt.Sprintf("[[REPLACE_IMAGE:%s]]", filename)
-}
-
-func toCenteredImage(s string) string {
-	return fmt.Sprintf("[[CENTERED_IMAGE:%s]]", s)
 }
 
 // preserveNewline newlines are treated as `SHIFT + ENTER` input,
